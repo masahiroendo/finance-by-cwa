5989b1d6680a6cc0f213ea9705a13fdfc1dff478
matcha <massahiro.endo@gmail.com>
2024-05-14 22:47:23 +0200

    feat: hono drizzle neon tanstack react-query
    hono error handling

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,3 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
-  return (
-    <div className="space-y-4">
-      <p>Hi this is a protected page accessible only by login in</p>
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  );
+  return <div>Home Page</div>;
 }
